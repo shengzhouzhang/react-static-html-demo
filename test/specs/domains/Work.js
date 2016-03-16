9a2330be4f3e3003dd49e620bbe66704475f09f3
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-16 21:57:12 +1100

    add test for Work domain

---
+++ test/specs/domains/Work.js
@@ -0,0 +1,22 @@
+
+import { expect } from 'chai';
+import Work from '../../../src/domains/Work';
+
+describe('Work Domain', () => {
+
+  it('should have propety _id, imageUrl, make, and model', () => {
+    let work = new Work();
+    expect(work).to.have.property('_id');
+    expect(work).to.have.property('imageUrl');
+    expect(work).to.have.property('make');
+    expect(work).to.have.property('model');
+  });
+
+  it('should transfer make and model to lower cases', () => {
+    const MAKE = 'MAKE';
+    const MODEL = 'MODEL';
+    let work = new Work('0', '', MAKE, MODEL);
+    expect(work.make).to.eql(MAKE.toLowerCase());
+    expect(work.model).to.eql(MODEL.toLowerCase());
+  });
+});
