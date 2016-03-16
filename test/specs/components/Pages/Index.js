89dc82e372d3c618d3cf08b83a9adb6dc6c3b029
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-16 23:05:19 +1100

    add test for page index component

---
+++ test/specs/components/Pages/Index.js
@@ -0,0 +1,36 @@
+
+import _ from 'lodash';
+import React from 'React';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import Header from '../../../../src/components/Header';
+import Thumbnails from '../../../../src/components/Thumbnails';
+import Index from '../../../../src/components/Pages/Index';
+
+describe('Index Component', () => {
+
+  describe('render function', () => {
+
+    it('should render header and thumbnails', () => {
+      const PROPS = { items: [
+        { _id: 'ID_1', imageUrl: 'URI_1', make: 'MAKE_1', model: 'MODEL_1' },
+        { _id: 'ID_2', imageUrl: 'URI_2', make: 'MAKE_2', model: 'MODEL_2' }
+      ] };
+      const EXPECTED_HEADER_PROPS = {
+        title: 'index',
+        navigations: [
+          { name: 'MAKE_1', uri: '/make/MAKE_1' },
+          { name: 'MAKE_2', uri: '/make/MAKE_2' }
+        ]
+      };
+      const EXPECTED_THUMBNAILS_PROPS = {
+        items: [ { imageUrl: 'URI_1' }, { imageUrl: 'URI_2' } ]
+      };
+      let wrapper = shallow(<Index {...PROPS} />);
+      expect(wrapper.find(Header)).to.have.length(1);
+      expect(wrapper.find(Header).props()).to.eql(EXPECTED_HEADER_PROPS);
+      expect(wrapper.find(Thumbnails)).to.have.length(1);
+      expect(wrapper.find(Thumbnails).props()).to.eql(EXPECTED_THUMBNAILS_PROPS);
+    });
+  });
+});
