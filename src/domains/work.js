47ff5077618b0dc6dfff3decb3bbadc491288868
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-22 10:46:40 +1100

    add iso to Work domain

--- src/domains/work.js
+++ src/domains/work.js
@@ -1,11 +1,12 @@
 
 export default class Work {
 
-  constructor (id, imageUrl, make, model) {
+  constructor (id, imageUrl, make, model, iso) {
 
     this._id = id;
     this.imageUrl = imageUrl;
     this.make = make && make.toLowerCase() || 'unknown';
     this.model = make && model.toLowerCase() || 'unknown';
+    this.iso = iso;
   }
 }
--- test/specs/domains/Work.js
+++ test/specs/domains/Work.js
@@ -4,12 +4,13 @@ import Work from '../../../src/domains/Work';
 
 describe('Work Domain', () => {
 
-  it('should have propety _id, imageUrl, make, and model', () => {
+  it('should have propety _id, imageUrl, make, model, and iso', () => {
     let work = new Work();
     expect(work).to.have.property('_id');
     expect(work).to.have.property('imageUrl');
     expect(work).to.have.property('make');
     expect(work).to.have.property('model');
+    expect(work).to.have.property('iso');
   });
 
   it('should transfer make and model to lower cases', () => {
